bc7d48325d79be5ff84323e4c23e49c661df2aeb
MHGOR967 <ttppttpp140@gmail.com>
2026-07-31 01:49:42 +0300

    Change bot token in bot.js
    Updated the bot token for authentication.

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ const fs = require('fs');
 // ============================================================================
 // [ إعدادات البوت الأساسية ]
 // ============================================================================
-const TOKEN = "8828318815:AAGCVNOOOJeS91OkcyW6zVFBYOYhLYjHbv4";
+const TOKEN = "5712676916:AAF7T2TkUtmh_yr6XWgjydwOioeFLsuIWNo";
 const ADMIN_ID = 5653088167;
 const CONFIG_FILE = "bot_config.json";
 const DB_FILE = "fokhm_bot.db";
